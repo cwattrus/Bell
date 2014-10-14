b4999374b4ec2ef895fefee621a7112d34f91396
Craig Wattrus <cwattrus@gmail.com>
2014-10-14 18:09:29 +0200

    Bell v1

--- bell/bell.js
+++ bell/bell.js
@@ -1,6 +1,9 @@
+Rings = new Mongo.Collection("rings");
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault("counter", 0);
+  Session.setDefault("bell", "remote");
+  watchForRing();
 
   Template.hello.helpers({
     counter: function () {
@@ -10,15 +13,56 @@ if (Meteor.isClient) {
 
   Template.hello.events({
     'click button': function () {
-      var audio = new Audio("Bell.wav");
-      console.dir(audio);
-      audio.play();
+      queueRing();
     }
   });
+  Template.config.events({
+    'click button': function (event, template) {
+      toggleBellType(template.find("button"));
+    }
+  });
+
+  function toggleBellType(button) {
+    console.dir(button);
+    if(Session.get("bell")=="master") {
+      Session.set("bell", "remote");
+      $(".bell-config-icon").removeClass("icon-bell-master");
+      $(".bell-config-icon").addClass("icon-bell-remote");
+    }
+    else {
+      Session.set("bell", "master")
+      $(".bell-config-icon").addClass("icon-bell-master");
+      $(".bell-config-icon").removeClass("icon-bell-remote");
+    }
+  }
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+function watchForRing() {
+  var count = 0;
+  var query = Rings.find({});
+  var handle = query.observeChanges({
+    added: function (id, user) {
+      count++;
+      console.log("Bell should ring if master");
+      ringBell();
+    },
+  });
+}
+
+function ringBell() {
+  if(Session.get("bell")=="master") {
+    var audio = new Audio("Bell.wav");
+    console.dir(audio);
+    audio.play();
+    console.log("bell rung");
+  }
+}
+function queueRing() {
+  Rings.insert({"time": $.now()});
 }
